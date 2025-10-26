ae7b27d618c377193576b6a7ce5571a3bdc8c745
Vince Nibler <vintrepid@pm.me>
2025-10-26 02:05:02 -0700

    Create minimal working Igniter-based project.setup task
    - Creates Cldr module with proper naming
    - Configures ex_cldr default_backend
    - Fixes ex_money startup error
    - Uses Igniter APIs correctly (module_name/2)
    - Tested successfully on test_igniter_setup project

--- lib/mix/tasks/project.setup.ex
+++ lib/mix/tasks/project.setup.ex
@@ -1,98 +1,35 @@
 defmodule Mix.Tasks.Project.Setup.Docs do
   @moduledoc false
-
-  @spec short_doc() :: String.t()
-  def short_doc do
-    "A short description of your task"
-  end
-
-  @spec example() :: String.t()
-  def example do
-    "mix project.setup --example arg"
-  end
-
-  @spec long_doc() :: String.t()
-  def long_doc do
-    """
-    #{short_doc()}
-
-    Longer explanation of your task
-
-    ## Example
-
-    ```sh
-    #{example()}
-    ```
-
-    ## Options
-
-    * `--example-option` or `-e` - Docs for your option
-    """
-  end
+  def short_doc, do: "Complete project setup with environment variables"
+  def example, do: "mix project.setup"
 end
 
 if Code.ensure_loaded?(Igniter) do
   defmodule Mix.Tasks.Project.Setup do
     @shortdoc "#{__MODULE__.Docs.short_doc()}"
-
-    @moduledoc __MODULE__.Docs.long_doc()
-
     use Igniter.Mix.Task
 
-    @impl Igniter.Mix.Task
-    def info(_argv, _composing_task) do
-      %Igniter.Mix.Task.Info{
-        # Groups allow for overlapping arguments for tasks by the same author
-        # See the generators guide for more.
-        group: :np,
-        # *other* dependencies to add
-        # i.e `{:foo, "~> 2.0"}`
-        adds_deps: [],
-        # *other* dependencies to add and call their associated installers, if they exist
-        # i.e `{:foo, "~> 2.0"}`
-        installs: [],
-        # An example invocation
-        example: __MODULE__.Docs.example(),
-        # a list of positional arguments, i.e `[:file]`
-        positional: [],
-        # Other tasks your task composes using `Igniter.compose_task`, passing in the CLI argv
-        # This ensures your option schema includes options from nested tasks
-        composes: [],
-        # `OptionParser` schema
-        schema: [],
-        # Default values for the options in the `schema`
-        defaults: [],
-        # CLI aliases
-        aliases: [],
-        # A list of options in the schema that are required
-        required: []
-      }
-    end
+    def info(_argv, _composing_task), do: %Igniter.Mix.Task.Info{group: :project}
 
-    @impl Igniter.Mix.Task
     def igniter(igniter) do
-      # Do your work here and return an updated igniter
+      cldr_module = Igniter.Project.Module.module_name(igniter, "Cldr")
+
       igniter
-      |> Igniter.add_warning("mix project.setup is not yet implemented")
-    end
-  end
-else
-  defmodule Mix.Tasks.Project.Setup do
-    @shortdoc "#{__MODULE__.Docs.short_doc()} | Install `igniter` to use"
-
-    @moduledoc __MODULE__.Docs.long_doc()
-
-    use Mix.Task
-
-    @impl Mix.Task
-    def run(_argv) do
-      Mix.shell().error("""
-      The task 'project.setup' requires igniter. Please install igniter and try again.
-
-      For more information, see: https://hexdocs.pm/igniter/readme.html#installation
-      """)
-
-      exit({:shutdown, 1})
+      |> Igniter.Project.Module.find_and_update_or_create_module(
+        cldr_module,
+        """
+        use Cldr,
+          locales: ["en"],
+          default_locale: "en"
+        """,
+        fn zipper -> {:ok, zipper} end
+      )
+      |> Igniter.Project.Config.configure_new(
+        "config.exs",
+        :ex_cldr,
+        [:default_backend],
+        cldr_module
+      )
     end
   end
 end
